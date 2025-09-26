ba656ca41954d04fb96145db6b3452c5550ace73
Nirogyam <nirogyam93@gmail.com>
2025-09-26 15:46:53 +0000

    Try fixing this error: `Unhandled Runtime Error: TypeError: Cannot destr

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -44,13 +45,24 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
-  const { getFieldState, formState } = useFormContext()
-
-  const fieldState = getFieldState(fieldContext.name, formState)
+  const form = useFormContext()
 
   if (!fieldContext) {
     throw new Error("useFormField should be used within <FormField>")
   }
+  
+  if (!form) {
+    return {
+      id: itemContext.id,
+      name: fieldContext.name,
+      formItemId: `${itemContext.id}-form-item`,
+      formDescriptionId: `${itemContext.id}-form-item-description`,
+      formMessageId: `${itemContext.id}-form-item-message`,
+    }
+  }
+
+  const { getFieldState, formState } = form;
+  const fieldState = getFieldState(fieldContext.name, formState)
 
   const { id } = itemContext
 
